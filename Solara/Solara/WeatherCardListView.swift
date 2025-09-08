c8391508e4faa358813bcebe739d26eaab919f21
zeynepcnky <zeynepa1472@gmail.com>
2025-09-08 18:34:11 +0300

    Refactor : Extract Card List View from Content View

---
+++ Solara/Solara/WeatherCardListView.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherCardListView.swift
+//  Solara
+//
+//  Created by Zeynep Cankaya on 5.09.2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct WeatherCardListView: View {
+    let cities: [WeatherItem]
+    let animation : Namespace.ID
+    let onSelect : (WeatherItem) -> Void
+    
+    var body: some View {
+        if !cities.isEmpty {
+            ScrollView {
+                VStack(spacing: 12) {
+                    ForEach(cities) { city in
+                        CardView(city: city.city, temperature: city.temp.current.temperature)
+                            .matchedGeometryEffect(id: city.id, in: animation)
+                            .onTapGesture { onSelect(city) }
+                        
+                    }
+                }
+                .padding(.vertical, 8)
+            }
+        }
+    }
+}
+
