b0c1fc26acf5b10f44ac376a8adf959f276bba49
zeynepcnky <zeynepa1472@gmail.com>
2025-10-23 13:20:22 +0300

    Settings Overlay

--- Solara/Solara/SettingsOverlay.swift
+++ Solara/Solara/SettingsOverlay.swift
@@ -4,45 +4,44 @@
 //
 //  Created by Zeynep Cankaya on 8.09.2025.
 //
-import Foundation
+
 import SwiftUI
 
 struct SettingsOverlay: View {
     @Binding var isVisible: Bool
-    @Binding var selectedUnit : Bool
-    internal let onEdit: () -> Void
-    
- 
-    var body: some View {
-        GeometryReader { geometry in
-            if isVisible {
+    @Binding var selectedUnit: Bool
+    let onEdit: () -> Void
 
-                ZStack {
-                    
-                    Color.white.opacity(0.3)
-                        .ignoresSafeArea()
-                        .onTapGesture {
-                            withAnimation {
-                                isVisible = false
-                            }
-                        }
-                }
-                        
-                    SettingsView(selectedUnit: $selectedUnit, onEdit: onEdit)
-                        .frame(width: 250, height: 220)
-                        .background(.regularMaterial)
-                        .cornerRadius(16)
-                        .shadow(radius: 5)
-                        .padding(.trailing, 60)
-                        .transition(.move(edge: .top).combined(with: .opacity))
-                        .position(x: geometry.size.width - 95, y: geometry.size.height - 650)
-                        .animation(.spring(), value: isVisible)
-                
-           }
-        }
+    var body: some View {
         
+        if isVisible {
+            
+            ZStack(alignment: .topTrailing) {
+                
+                
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        withAnimation {
+                            isVisible = false
+                        }
+                    }
+                
+                
+                SettingsView(selectedUnit: $selectedUnit, onEdit: onEdit)
+                    .frame(width: 250, height: 220)
+                    .background(.regularMaterial)
+                    .cornerRadius(16)
+                    .shadow(radius: 5)
+                    .zIndex(1)
+                    .padding(.top, -5)
+                    .padding(.trailing, 20)
+                    .transition(.move(edge: .top).combined(with: .opacity))
+            }
+           
+            .animation(.spring(), value: isVisible)
+        }
     }
-    
 }
 
 
