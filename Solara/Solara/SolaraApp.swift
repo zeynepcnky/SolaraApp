326388a6ea783312cfc5ec213f1790b12492cd55
zeynepcnky <zeynepa1472@gmail.com>
2025-10-23 13:19:40 +0300

    Configure SwiftData model container.

--- Solara/Solara/SolaraApp.swift
+++ Solara/Solara/SolaraApp.swift
@@ -10,11 +10,28 @@ import SwiftData
 
 @main
 struct SolaraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        do{
+            let schema = Schema([
+                WeatherData.self,
+                Current.self,
+                Hourly.self,
+                Daily.self
+            ])
+            let configuration = ModelConfiguration (
+                schema: schema,
+                isStoredInMemoryOnly: false
+            )
+            return try ModelContainer(for: schema, configurations: [configuration])
+        }catch {
+            fatalError("ModelContainer could not be initialized")
+        }
+    }()
     var body: some Scene {
         WindowGroup {
             ContentView()
-                
+             
         }
-        .modelContainer(for: [WeatherData.self, Current.self, Hourly.self, Daily.self])
+        .modelContainer(sharedModelContainer)
     }
 }
