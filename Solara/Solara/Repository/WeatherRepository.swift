cba95c660048366a763462ce284c0af616ea41ba
zeynepcnky <zeynepa1472@gmail.com>
2025-10-23 17:10:52 +0300

    Weather Repository

--- Solara/Solara/Repository/WeatherRepository.swift
+++ Solara/Solara/Repository/WeatherRepository.swift
@@ -16,98 +16,43 @@ class WeatherRepository {
         self.context = context
     }
     
-    func mapAPIDatatoWeatherData( apiData: APIWeatherData, city: String) -> WeatherData {
-        let current = Current(weatherCode: apiData.current.weatherCode,
-                              temperature: apiData.current.temperature,
-                              rain: apiData.current.rain,
-                              showers: apiData.current.showers,
-                              windSpeed: apiData.current.windSpeed,
-                              snowfall: apiData.current.snowfall,
-                              windDirection: apiData.current.windDirection)
-        
-        let hourly : [Hourly] = (0..<apiData.hourly.time.count).compactMap { i in
-            let timeString = apiData.hourly.time[i]
-            guard let forecastDate = WeatherFormatter.inputHourFormatter.date(from: timeString) else { return nil}
-            
-            return Hourly(
-                weatherCode: apiData.hourly.weatherCode[i],
-                date: forecastDate,
-                temperature: apiData.hourly.temperature[i],
-                showers: apiData.hourly.showers[i],
-                snowfall: apiData.hourly.snowfall[i],
-                windSpeed: apiData.hourly.windSpeed[i],
-                rain: apiData.hourly.rain[i] )
-        }
-        
-        let daily : [Daily] = (0..<apiData.daily.time.count).compactMap { i in
-            let timeString = apiData.daily.time[i]
-            
-            guard let forecastDate = WeatherFormatter.inputDateFormatter.date(from: timeString)else { return nil }
-            
-            return Daily(weatherCode: apiData.daily.weatherCode[i],
-                         date: forecastDate,
-                         temperatureMax: apiData.daily.temperatureMax[i],
-                         temperatureMin: apiData.daily.temperatureMin[i])
-        }
-        
-        let weatherData = WeatherData(city: city, timeZoneIdentifier: apiData.timeZone, current: current, hourly: hourly, daily: daily)
-        
-        current.weatherData = weatherData
-        
-        hourly.forEach { $0.weatherData = weatherData}
-        
-        daily.forEach { $0.weatherData = weatherData}
-        
-        return weatherData
-    }
+    func save() throws { try context.save() }
     
-    
+   
     func fetchAllCities() throws -> [WeatherData] {
-        let descriptor = FetchDescriptor<WeatherData>( sortBy: [SortDescriptor(\WeatherData.city)] )
-        
+        let descriptor = FetchDescriptor<WeatherData>( sortBy: [SortDescriptor(\.city)] )
         return try self.context.fetch(descriptor)
     }
     
-    func createAndSaveCity(from apiData: APIWeatherData, cityName: String) async -> WeatherData? {
-        let weatherDataToSave = self.mapAPIDatatoWeatherData(apiData: apiData, city: cityName)
+    
+    func createCity(from apiData: APIWeatherData, coordinate: GeocodeResult) async -> WeatherData {
+        let weatherDataToSave = WeatherData.fromAPI(apiData: apiData, coordinate: coordinate )
+        self.context.insert(weatherDataToSave)
         
-        do {
-            self.context.insert(weatherDataToSave)
-            
-            try self.context.save()
-            
-            print("✅ City data (\(cityName)) successfully saved.")
-            
-            return weatherDataToSave
-        }
-        catch { print("❌ Save error: \(error)")
-                return nil }
+        print("✅ (\(coordinate.name)) inserted into context.")
+        
+        return weatherDataToSave
     }
     
     func update(existingCity: WeatherData, with apiData: APIWeatherData) {
         
-        appplyAPIData(to: existingCity, from: apiData)
-        do{
-            try context.save()
-            print("🔄 \(existingCity.city) data updated and saved.")
-        }
-        catch{print("❌ Update and save error: \(error.localizedDescription)")}
+        applyAPIData(to: existingCity, from: apiData)
+        
+        print("🔄 \(existingCity.city) data updated in context.")
+    }
+    
+
+    func delete(city: WeatherData)  {
+        
+        self.context.delete(city)
+        
+        print("✅ City \(city.city) successfully deleted.")
     }
     
     
-    func delete(city: WeatherData) throws {
-        do {
-            self.context.delete(city)
-            
-            try self.context.save()
-            
-            print("✅ City \(city.city) successfully deleted.") }
-        catch { print("❌ Error: Something went wrong while deleting city. \(error)")
-                throw error }
-    }
-    
-    private func appplyAPIData(to weatherData: WeatherData, from apiData: APIWeatherData) {
+    private func applyAPIData(to weatherData: WeatherData, from apiData: APIWeatherData) {
         weatherData.timeZoneIdentifier = apiData.timeZone
+        
         if let current = weatherData.current {
             current.weatherCode = apiData.current.weatherCode
             current.temperature = apiData.current.temperature
@@ -117,17 +62,25 @@ class WeatherRepository {
             current.snowfall = apiData.current.snowfall
             current.windDirection = apiData.current.windDirection
         }
-        
+       
         weatherData.hourly.removeAll()
         
         let newHourly: [Hourly] = (0..<apiData.hourly.time.count).compactMap { i in
             let timeString = apiData.hourly.time[i]
-            guard let forecastDate = WeatherFormatter.inputHourFormatter.date(from: timeString) else { return nil
-            }
-            let hour = Hourly(weatherCode: apiData.hourly.weatherCode[i], date: forecastDate, temperature: apiData.hourly.temperature[i], showers: apiData.hourly.showers[i], snowfall: apiData.hourly.snowfall[i], windSpeed: apiData.hourly.windSpeed[i], rain: apiData.hourly.rain[i])
+            guard let forecastDate = WeatherFormatter.inputHourFormatter.date(from: timeString) else { return nil }
+           
+            let hour = Hourly(weatherCode: apiData.hourly.weatherCode[i],
+                              date: forecastDate,
+                              temperature: apiData.hourly.temperature[i],
+                              showers: apiData.hourly.showers[i],
+                              snowfall: apiData.hourly.snowfall[i],
+                              windSpeed: apiData.hourly.windSpeed[i],
+                              rain: apiData.hourly.rain[i])
+            
             hour.weatherData = weatherData
             return hour
         }
+        
         weatherData.hourly = newHourly
         
         weatherData.daily.removeAll()
@@ -137,14 +90,15 @@ class WeatherRepository {
             guard let forecastDate = WeatherFormatter.inputDateFormatter.date(from: timeString) else {
                 return nil
             }
-            let day = Daily(weatherCode: apiData.daily.weatherCode[i], date: forecastDate, temperatureMax: apiData.daily.temperatureMax[i], temperatureMin: apiData.daily.temperatureMin[i])
+            let day = Daily(weatherCode: apiData.daily.weatherCode[i],
+                            date: forecastDate,
+                            temperatureMax: apiData.daily.temperatureMax[i],
+                            temperatureMin: apiData.daily.temperatureMin[i])
+          
             day.weatherData = weatherData
             return day
         }
-        weatherData.daily = newDaily
-        
-        print("🔄 \(weatherData.city) data updated.")
-        
+       weatherData.daily = newDaily
     }
 }
 
