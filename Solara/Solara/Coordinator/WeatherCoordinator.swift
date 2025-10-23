b7493dc9f9846be5b73f9ea74a99355eb507d928
zeynepcnky <zeynepa1472@gmail.com>
2025-10-23 15:47:15 +0300

    Weather Coordinator.

--- Solara/Solara/Coordinator/WeatherCoordinator.swift
+++ Solara/Solara/Coordinator/WeatherCoordinator.swift
@@ -5,195 +5,63 @@
 //  Created by Zeynep Cankaya on 5.09.2025.
 //
 import Foundation
+import SwiftUI
 import SwiftData
-
+import Combine
 
 @MainActor
 final class WeatherCoordinator: ObservableObject {
-    @Published var cityName : String = ""
-    @Published var isSearchableActive : Bool = false
-    @Published var addedCity : [WeatherData] = []
-    @Published var tempWeatherData : WeatherData?
-    @Published var isSheetActive : Bool = false
-    @Published var selectedWeather : WeatherData?
-    @Published var latitude : Double = 0.0
-    @Published var longitude : Double = 0.0
-    @Published var isLocationActive : Bool = false
+        
+  @ObservedObject var cityListViewModel = CityListViewModel()
+  @ObservedObject var viewModel =  WeatherSearchViewModel()
     
-    let viewModel = WeatherViewModel()
+    @Published var searchedCityDetail : WeatherData?
+    @Published var selectedCityForDetail : WeatherData?
+
+    private var isSearchActive : Binding<Bool>?
+    private var lastGeocodeResult : GeocodeResult?
+    private let service = WeatherService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(){
+        cityListViewModel.objectWillChange.sink { [weak self]_ in
+            self?.objectWillChange.send()
+        }
+        .store(in: &cancellables)
+        }
     
     
-    private var repository : WeatherRepository?
-    private let lastSelectedCityKey = "lastSelectedCityName"
-    private var updateTask : Task<Void, Never>? = nil
-    private let updateTime : TimeInterval = 600
-    
-    func setupContext(_ context: ModelContext){
-        self.repository = WeatherRepository(context: context)
-        loadSavedCities()
-        startUpdateLoop()
+    func setup(with context: ModelContext, isSearchActive: Binding<Bool>?){
+        cityListViewModel.setupContext(context)
+        self.isSearchActive = isSearchActive
     }
-    
-    func startSearch() {
-        isSearchableActive = true
+  
+
+    func selectCityFromSearch(city : GeocodeResult) {
+        self.lastGeocodeResult = city
         Task {
-            defer{
-                isSearchableActive = false
+            do {
+              let apiData = try await self.service.fetchWeather(latitude: city.latitude, longitude: city.longitude)
+                let previewData = WeatherData.fromAPI(apiData: apiData, coordinate: city)
+                self.searchedCityDetail = previewData
             }
-            
-            await viewModel.getWeather(cityName: cityName)
-            
-            if let apiData = viewModel.weather {
-                self.tempWeatherData = repository?.mapAPIDatatoWeatherData(apiData: apiData, city: cityName)
-                isSheetActive = true
-                
-            }
+            catch { print("❌ Error: Failed to fetch search details. - \(error)") }
         }
     }
     
-    func addCity() async{
-        
-        stopUpdateLoop()
-        guard let cityToSave = tempWeatherData,
-              let apiData = viewModel.weather else { startUpdateLoop()
-            return }
-        
-        if addedCity.contains(where: {$0.city.lowercased() == cityToSave.city}) {
-            isSheetActive = false
-            startUpdateLoop()
+    func addLastSearchedCity(){
+        guard let cityToAdd = lastGeocodeResult else {
+            print("❌ Error: Found no city to add.")
             return
         }
-        if let savedCity = await repository?.createAndSaveCity(from: apiData, cityName: cityToSave.city) {
-            addedCity.append(savedCity)
-        } else{
-            print("❌ City could not be saved to the database.")
-        }
-        
-        
-        startUpdateLoop()
-        isSheetActive = false
-        isSearchableActive = false
-        tempWeatherData = nil
-        viewModel.weather = nil
-        
-    }
-    
-    
-    func startUpdateLoop(){
-        updateTask?.cancel()
-        
-        updateTask = Task {
-            while !Task.isCancelled {
-                print("🔄 Starting: Updating all city...")
-                await fetchUpdatesForAllCities()
-                
-                do {
-                    try await Task.sleep(for: .seconds(updateTime))
-                }catch{
-                    print("Task was cancelled")
-                    break
-                }
-            }
+        Task {
+            await cityListViewModel.fetchWeather(city: cityToAdd)
+            
+            self.searchedCityDetail = nil
+            self.isSearchActive?.wrappedValue = false
         }
     }
-    private func fetchUpdatesForAllCities() async {
-        guard let repository = self.repository else { return }
-        
-        let cityNames = self.addedCity.map {$0.city}
-           
-
-        
-        let backgroundContext = ModelContext(repository.context.container)
-        let backgrounRepo = WeatherRepository(context: backgroundContext)
-        
-        for cityName in cityNames {
-            
-            
-            await viewModel.getWeather(cityName: cityName)
-            
-            guard let newAPIData = viewModel.weather else {
-                print("Update failed: for \(cityName) can not fetch the API data.")
-                continue
-            }
-            
-            let predicate = #Predicate<WeatherData> { $0.city == cityName }
-            let descriptor = FetchDescriptor(predicate: predicate)
-            
-            do {
-                if let cityToUpdate = try backgroundContext.fetch(descriptor).first {
-                    
-                    backgrounRepo.update(existingCity: cityToUpdate, with: newAPIData)
-                }
-            } catch {
-                print("❌ Error fetching city to update: \(error)")
-            }
-            try? await Task.sleep(for: .milliseconds(500))
-        }
-        
-        
-        do {
-            try backgroundContext.save()
-        } catch {
-            print("❌ Error saving background context: \(error)")
-        }
-        
-        await MainActor.run {
-            self.loadSavedCities()
-            print("✅ Update cycle successfully ending.")
-        }
-    }
-
-
-    
-    func loadSavedCities(){
-        guard let repository = self.repository else { return }
-        
-        do{
-            self.addedCity = try repository.fetchAllCities()
-           }catch{ print("❌ Şehirler yüklenirken hata oluştu: \(error.localizedDescription)")}
-    }
-
-    func select(_ city: WeatherData) {
-        selectedWeather = city
-        UserDefaults.standard.set(city.city, forKey: lastSelectedCityKey)
-    }
-    
-    func confirmSelected() {
-        selectedWeather = nil
-    }
-    
-    func deleteCity(at offsets: IndexSet) {
-        guard let repository = self.repository else { return }
-        
-        stopUpdateLoop()
-        
-        let citiesToDelete = offsets.map { addedCity[$0] }
-        
-        do{
-            for city in citiesToDelete {
-                try repository.delete(city: city)
-            }
-            addedCity.remove(atOffsets: offsets)
-            startUpdateLoop()
-        } catch{ print("❌ Cities could not delete from database: \(error)")}
-            
-        startUpdateLoop()
-    }
-    func moveCity(from source : IndexSet, to destination: Int) {
-        addedCity.move(fromOffsets: source, toOffset: destination)
-    }
-    
-        
-    }
-    
-
-    extension WeatherCoordinator {
-        func stopUpdateLoop() {
-            updateTask?.cancel()
-        updateTask = nil
-        print("⏸ Güncelleme görevi durduruldu.")
-    }
-}
+  }
 
     
 
