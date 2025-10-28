32648d4ff3d2622021ddf95c86324c0d344cc4be
zeynepcnky <zeynepa1472@gmail.com>
2025-10-28 14:08:11 +0300

    Update assets.

---
+++ Solara/Solara/Formatter/DateParsing.swift
@@ -0,0 +1,13 @@
+//
+//  DateParsing.swift
+//  Solara
+//
+//  Created by Zeynep Cankaya on 27.10.2025.
+//
+
+import SwiftData
+import Foundation
+
+protocol DateParsing {
+    func date(from string: String) -> Date?
+}
